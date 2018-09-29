0f30b0622ddc7b9917dc3149c4c50d8bd4118565
Moses Lee <leecmoses@gmail.com>
2018-09-28 22:36:25 -0400

    Completed Lesson 10

--- 10-how-to-solve-problems/notes.py
+++ 10-how-to-solve-problems/notes.py
@@ -15,6 +15,7 @@ Pythonista's Guide to All Problems in the Galaxy
 2. What are the outputs?
 3. Work through some examples by hand
 4. Simple mechanical solution
+5. Develop incrementally and test as we go!
 * work out examples to understand the relationship between inputs and outputs
 * consider systematically how a human solves the problem
 * write in pseudocode
@@ -36,11 +37,49 @@ Pythonista's Guide to All Problems in the Galaxy
 #     return year, month, day
 
 #  Alternatively
+def isLeapYear(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False
+
+def daysInMonth(year, month):
+    if month in (1, 3, 5, 7, 8, 10, 12):
+        return 31
+    else:
+        if month == 2:
+            if isLeapYear(year):
+                return 29
+            else:
+                return 28
+    return 30
+
 def nextDay(year, month, day):
-    if day < 30:
+    if day < daysInMonth(year, month):
         return year, month, day + 1
     else:
-        if month < 12:
+        if month == 12:
             return year, month + 1, 1
         else:
             return year + 1, 1, 1
+
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        day += 1
+    return days
