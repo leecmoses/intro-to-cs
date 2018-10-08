f55f151b959a76b34a4757cc6bc5b5ecc0751ae2
Moses Lee <leecmoses@gmail.com>
2018-10-08 14:55:17 -0400

    Completed Lessons 25 & 26

---
+++ 25-qna/notes.py
@@ -0,0 +1,12 @@
+#############
+#   Notes   #
+# Lesson 25 #
+#############
+'''
+* 'Pythonic' way is doing things the best way in Python
+* More people are using Python 3 than Python 2 in recent years (2018)
+    * ref - https://learntocodewith.me/programming/python/python-2-vs-python-3/
+* Usually while loop is more efficient but a recursion function is more elegant (easier to read, understand, and follow).
+    * However if a language does provide tail recursion elimination than recursion functions may be better to use.
+
+'''
---
+++ 26-past-present-and-future-of-computing/notes.py
@@ -0,0 +1,22 @@
+#############
+#   Notes   #
+# Lesson 26 #
+#############
+'''
+Themes of computer science
+    * abstraction
+        - hiding details
+    * universality
+        - way to make decisions 'if'
+        - way to keep track of variables, data
+        - way to keep going using while -> procedures(recursive)
+    * recursive definitions
+        - Expr -> Expr + Expr
+        - Expr -> Number
+
+Opensource
+    * Means the source code is openly available and the code can be altered or modified
+
+Quantum Computing
+    * bits are just 0s and 1s, interestingly qubits are in a superposition until a measurement is taken
+'''
