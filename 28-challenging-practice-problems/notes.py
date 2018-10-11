e96c9ee1092289fdfd0202cbaf1f6d6b269e448d
Moses Lee <leecmoses@gmail.com>
2018-10-11 09:21:33 -0400

    Started Lesson 28

---
+++ 28-challenging-practice-problems/notes.py
@@ -0,0 +1,17 @@
+#############
+#   Notes   #
+# Lesson 28 #
+#############
+'''
+Quiz: Stirling and Bell
+'''
+
+
+'''
+Quiz: Combating Link Spam
+'''
+
+
+'''
+Quiz: Elementary Cellular Automation
+'''
