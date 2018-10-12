09559f091cab966d4efea0486394dd31f4a9f8d7
Moses Lee <leecmoses@gmail.com>
2018-10-12 09:38:57 -0400

    Completed Stirling and Bell of Lesson 28

--- 28-challenging-practice-problems/notes.py
+++ 28-challenging-practice-problems/notes.py
@@ -5,7 +5,18 @@
 '''
 Quiz: Stirling and Bell
 '''
+def stirling(n, k):
+    if n < k:
+        return 0
+    if n == k or k == 1:
+        return 1
+    return k*stirling(n-1, k) + stirling(n-1, k-1)
 
+def bell(n):
+    total = 0
+    for k in range(1, n+1):
+        total = total + stirling(n,k)
+    return total
 
 '''
 Quiz: Combating Link Spam
