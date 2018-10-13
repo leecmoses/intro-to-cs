ebf67f1405251223704ff5d45595a3b511b1c051
Moses Lee <leecmoses@gmail.com>
2018-10-13 09:58:05 -0400

    Completed Lesson 28

--- 28-challenging-practice-problems/notes.py
+++ 28-challenging-practice-problems/notes.py
@@ -55,3 +55,25 @@ def compute_ranks(graph, k):
 '''
 Quiz: Elementary Cellular Automation
 '''
+def cellular_automaton(string, pattern_number, generations):
+    patterns = {}
+    pattern_list = ['...', '..x', '.x.', '.xx', 'x..', 'x.x', 'xx.', 'xxx']
+    n = len(string)
+
+    # build my patterns dictionary
+    for i in range(7, -1, -1):
+        if pattern_number/(2**i) == 1:
+            patterns[pattern_list[i]] = 'x'
+            pattern_number = pattern_number - 2**i
+        else:
+            patterns[pattern_list[i]] = '.'
+    
+    # make a new string by applying pattern to string
+    # generations times
+    for j in range(0, generations):
+        new_string = ''
+        for i in range(0, n):
+            pattern = string[i-1] + string[i] + string[(i+1)%n]
+            new_string = new_string + patterns[pattern]
+        string = new_string
+    return new_string
