dc8e7ff85d46efda10854bc500e8c8713ea7076e
Moses Lee <leecmoses@gmail.com>
2018-10-14 10:02:52 -0400

    Link to code editor

---
+++ 29-code-editor/practice.py
@@ -0,0 +1,12 @@
+#############
+#   Notes   #
+# Lesson 29 #
+#############
+'''
+https://classroom.udacity.com/courses/cs101/lessons/299053470/concepts/2989296500923
+
+# Use this to try out anything you like. Use print to display your answer
+# when you press the "Test Run" button.
+# Use the "Reset" button to reset the screen 
+
+'''
